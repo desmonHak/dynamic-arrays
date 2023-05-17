6263281698b9c2ea548bed4fa1247fc4dd2c91ba
Desmon <61366533+desmonHak@users.noreply.github.com>
2023-05-17 21:28:04 +0200

    matriz code code.c

--- code.c
+++ code.c
@@ -1,11 +1,63 @@
 #include "vector-list.h"
 
 int main() {
+
+    // Crear una matriz 3x3 utilizando la lista enlazada
+    int rows = 3;
+    int columns = 3;
+
+    // Crear una lista enlazada de listas enlazadas
+    LinkedList *matrix = createLinkedList();
+
+    // Inicializar cada fila de la matriz como una lista enlazada
+    for (int i = 0; i < rows; i++) {
+        LinkedList *row = createLinkedList();
+        insertNode(matrix, row);
+    }
+
+    // Rellenar la matriz con algunos datos de ejemplo
+    for (int i = 0; i < rows; i++) {
+        LinkedList *row = (LinkedList *)get_element_v(matrix, i);
+        for (int j = 0; j < columns; j++) {
+            int *data = (int *)malloc(sizeof(int));
+            *data = i * columns + j; // Valor de ejemplo
+            insertNode(row, data);
+        }
+    }
+
+    // Acceder a los elementos de la matriz y mostrar su contenido
+    for (int i = 0; i < rows; i++) {
+        LinkedList *row = (LinkedList *)get_element_v(matrix, i);
+        Node *current = row->head;
+        while (current != NULL) {
+            int *data = (int *)current->data;
+            printf("%d ", *data);
+            current = current->next;
+        }
+        printf("\n");
+    }
+
+    // Liberar la memoria de la matriz y sus elementos
+    for (int i = 0; i < rows; i++) {
+        LinkedList *row = (LinkedList *)get_element_v(matrix, i);
+        Node *current = row->head;
+        while (current != NULL) {
+            Node *temp = current;
+            current = current->next;
+            free(temp->data); // Liberar memoria del elemento
+            //free(temp);
+        }
+        freeLinkedList(row); // Liberar memoria de la lista enlazada
+    }
+    freeLinkedList(matrix); // Liberar memoria del vector de listas enlazadas
+
+
+
     LinkedList* list = createLinkedList();
 
     element_def_asg(int, n1, 10)
-    element_def_asg(int, n2, 20)
-    element_def_asg(int, n3, 30)
+    element_def_asg(float, n2, 20.2)
+    element_def_asg(char, n3, 'l')
     element_def_asg(int, n4, 50)
     element_def_asg(int, n5, 60)
 
@@ -15,7 +67,9 @@ int main() {
     insertNode(list, n4);
     insertNode(list, n5);
 
-    printf("%d\n", get_elment(int, list, 1));
+    printf("%d\n", get_element(int, list, 0));
+    printf("%f\n", get_element(float, list, 1));
+    printf("%c\n", get_element(char, list, 2));
 
     printf("Lista enlazada: ");
     printLinkedList(list);
