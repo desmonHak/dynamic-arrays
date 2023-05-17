01fef56626af1d3ff304e71ac961c021c42d70cc
Desmon <61366533+desmonHak@users.noreply.github.com>
2023-05-17 23:26:30 +0200

    Update vector-list.c

--- vector-list.c
+++ vector-list.c
@@ -3,11 +3,86 @@
 
 #include "vector-list.h"
 
+void __attribute__((constructor)) __constructor_array_dinamic__()
+{
+    table_matriz_ = (LinkedList *)malloc(sizeof(LinkedList));
+    table_matriz_->head = NULL;
+    table_matriz_->lastId = 0; // al crearse la lista la posicion incial es 0
+}
+void __attribute__((destructor)) __destructor_array_dinamic__()
+{
+#ifdef __VECTOR_LIST_DEBBUG__
+    puts("liberando");
+#endif
+
+    // Liberar los vectores almacenados en table_matriz_
+    Node *current = table_matriz_->head;
+    while (current != NULL && !empty(table_matriz_))
+    {
+        Node *next = current->next;
+        LinkedList *vector = (LinkedList *)current->data;
+
+        if (vector != NULL)
+        {
+            deleteNodeID(table_matriz_, current->id); // Eliminar el nodo de la tabla de vectores
+
+            if (vector->head != NULL)
+            {
+                freeLinkedList(vector); // Liberar el vector
+            }
+        }
+
+        current = next;
+    }
+#ifdef __VECTOR_LIST_DEBBUG__
+    puts("vectores liberados");
+#endif
+    // Liberar table_matriz_ en sí
+    freeLinkedList(table_matriz_); // Liberar la tabla de vectores
+#ifdef __VECTOR_LIST_DEBBUG__
+    puts("Tabla de vectores liberada exitosamente");
+#endif
+}
+
+Node *get_node(LinkedList *list, position pos)
+{
+    if (list == NULL || pos < 0)
+    {
+        return NULL; // Lista inválida o posición inválida
+    }
+
+    Node *current = list->head;
+    while (current != NULL && pos > 0)
+    {
+        current = current->next;
+        pos--;
+    }
+
+    return current;
+}
+
+position get_position(Node *node)
+{
+    if (node == NULL)
+    {
+        return -1; // Nodo nulo, posición inválida
+    }
+    return node->id;
+}
+
 LinkedList *createLinkedList()
 {
     LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
     list->head = NULL;
     list->lastId = 0; // al crearse la lista la posicion incial es 0
+
+    // insertamos una referencia de un vector en la tabla para llevar el registro:
+    insertNode(table_matriz_, list);
+#ifdef __VECTOR_LIST_DEBBUG__
+    printf("Registro de vectores reservados en run time (table_matriz_): ");
+    printLinkedList(table_matriz_);
+#endif
+
     return list;
 }
 
@@ -25,7 +100,7 @@ void *get_element_v(LinkedList *list, const position ID)
     return NULL; // ID no encontrado
 }
 
-void insertNode(LinkedList* list, void *data)
+void insertNode(LinkedList *list, void *data)
 {
     Node *newNode = (Node *)malloc(sizeof(Node));
     newNode->id = list->lastId++;
@@ -46,15 +121,81 @@ void insertNode(LinkedList* list, void *data)
         current->next = newNode;
     }
 }
+bool existsID(LinkedList *list, const position ID)
+{
+    Node *current = list->head;
+    while (current != NULL)
+    {
+        if (current->id == ID)
+        {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
 
-void deleteNode(LinkedList* list, const position ID)
+bool exists(LinkedList *list, Node *node)
+{
+    Node *current = list->head;
+    while (current != NULL)
+    {
+        if (current == node)
+        {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+void deleteNode(LinkedList *list, Node *node)
+{
+    if (list->head == NULL || node == NULL)
+    {
+        printf("La lista o el nodo son nulos.\n");
+        return;
+    }
+
+    if (list->head == node)
+    {
+        list->head = list->head->next;
+        free(node);
+        updateIds(list);
+        return;
+    }
+
+    Node *prev = list->head;
+    Node *current = list->head->next;
+
+    while (current != NULL && current != node)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current != NULL)
+    {
+        prev->next = current->next;
+        free(current);
+        updateIds(list);
+    }
+    else
+    {
+        printf("El nodo no se encontro en la lista.\n");
+    }
+}
+
+void deleteNodeID(LinkedList *list, const position ID)
 {
     if (list->head == NULL)
     {
+#ifdef __VECTOR_LIST_DEBBUG__
         printf("La lista esta vacia.\n");
+#endif
         return;
     }
-if (list->head->id == ID)
+    if (list->head->id == ID)
     {
         Node *temp = list->head;
         list->head = list->head->next;
@@ -80,9 +221,10 @@ if (list->head->id == ID)
     }
     else
     {
+#ifdef __VECTOR_LIST_DEBBUG__
         printf("El nodo con ID %d no se encontro en la lista.\n", ID);
+#endif
     }
-
 }
 
 void updateIds(LinkedList *list)
@@ -94,7 +236,6 @@ void updateIds(LinkedList *list)
         current->id = list->lastId++;
         current = current->next;
     }
-    
 }
 
 position pop_back(LinkedList *list)
@@ -168,7 +309,6 @@ void clear(LinkedList *list)
     list->lastId = 0;
 }
 
-
 position size(LinkedList *list)
 {
     size_t count = 0;
@@ -180,7 +320,6 @@ position size(LinkedList *list)
     }
     return count;
 }
-
 
 void printLinkedList(LinkedList *list)
 {
@@ -195,11 +334,25 @@ void printLinkedList(LinkedList *list)
 
 void freeLinkedList(LinkedList *list)
 {
+#ifdef __VECTOR_LIST_DEBBUG__
+    printf("Registro de vectores reservados en run time (table_matriz_): ");
+    printLinkedList(table_matriz_);
+#endif
+
+    if (list == NULL)
+        return;
+
     Node *current = list->head;
     while (current != NULL)
     {
+#ifdef __VECTOR_LIST_DEBBUG__
+        printf("freeLinkedList: %zu\n", current->id);
+#endif
         Node *temp = current;
         current = current->next;
+        temp->data = NULL;
+        temp->next = NULL;
+        deleteNodeID(table_matriz_, temp->id); // Eliminar el nodo de table_matriz_
         free(temp);
     }
     free(list);
